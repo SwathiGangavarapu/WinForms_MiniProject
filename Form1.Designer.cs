105af0af0f0eef5d3a1fd0cc9d9c7ee0460f4a0a
SwathiGangavarapu <sgangavarapu@ghx.com>
2024-06-11 22:17:22 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace EmployeeManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridViewEmployees = new System.Windows.Forms.DataGridView();
+            this.btnEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(25, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EmployeeID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(25, 115);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(25, 162);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Department";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(25, 208);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Designation";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.BackColor = System.Drawing.SystemColors.Window;
+            this.txtEmployeeID.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtEmployeeID.Location = new System.Drawing.Point(178, 61);
+            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(168, 22);
+            this.txtEmployeeID.TabIndex = 5;
+            this.txtEmployeeID.TextChanged += new System.EventHandler(this.txtEmployeeID_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtName.Location = new System.Drawing.Point(178, 107);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(168, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtDepartment.Location = new System.Drawing.Point(178, 158);
+            this.txtDepartment.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(168, 22);
+            this.txtDepartment.TabIndex = 8;
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtDesignation.Location = new System.Drawing.Point(178, 204);
+            this.txtDesignation.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(168, 22);
+            this.txtDesignation.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAdd.ForeColor = System.Drawing.Color.Black;
+            this.btnAdd.Location = new System.Drawing.Point(38, 294);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(110, 45);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Create";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(38, 362);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(110, 46);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSearch.ForeColor = System.Drawing.Color.Black;
+            this.btnSearch.Location = new System.Drawing.Point(224, 362);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(110, 46);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridViewEmployees
+            // 
+            this.dataGridViewEmployees.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridViewEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployees.Location = new System.Drawing.Point(448, 59);
+            this.dataGridViewEmployees.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewEmployees.Name = "dataGridViewEmployees";
+            this.dataGridViewEmployees.RowHeadersWidth = 51;
+            this.dataGridViewEmployees.Size = new System.Drawing.Size(583, 411);
+            this.dataGridViewEmployees.TabIndex = 15;
+            this.dataGridViewEmployees.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEmployees_CellContentClick);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnEdit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEdit.Location = new System.Drawing.Point(224, 294);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(110, 45);
+            this.btnEdit.TabIndex = 16;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 551);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.dataGridViewEmployees);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridViewEmployees;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace EmployeeManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadEmployees();
+        }
+       private void LoadEmployees()
+        {
+            string query = "SELECT * FROM GHX_EMPLOYEESINFO";
+            DataTable dataTable = new DataTable() ;
+            dataGridViewEmployees.DataSource = dataTable;
+        }
+ 
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=10.9.176.202;Initial Catalog=NORTHWND;User ID=NORTHWND;Password=133tcru@NOS");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO GHX_EMPLOYEESINFO VALUES (@EmployeeID, @EmpName, @Department, @Designation)", conn);
+            cmd.Parameters.AddWithValue("@EmployeeID", int.Parse(txtEmployeeID.Text));
+            cmd.Parameters.AddWithValue("@EmpName", txtName.Text);
+            cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+            cmd.Parameters.AddWithValue("@Designation", txtDesignation.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Successfully Added");
+        }
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=10.9.176.202;Initial Catalog=NORTHWND;User ID=NORTHWND;Password=133tcru@NOS");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE GHX_EMPLOYEESINFO SET EmpName = @EmpName, Department = @Department, Designation = @Designation WHERE EmployeeID = @EmployeeID", conn);
+            cmd.Parameters.AddWithValue("@EmployeeID", int.Parse(txtEmployeeID.Text));
+            cmd.Parameters.AddWithValue("@EmpName", txtName.Text);
+            cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+            cmd.Parameters.AddWithValue("@Designation", txtDesignation.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Updated Successfully");
+         
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=10.9.176.202;Initial Catalog=NORTHWND;User ID=NORTHWND;Password=133tcru@NOS");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE GHX_EMPLOYEESINFO WHERE EmployeeID = @EmployeeID", conn);
+            cmd.Parameters.AddWithValue("@EmployeeID", int.Parse(txtEmployeeID.Text));
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Deleted Successfully");
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=10.9.176.202;Initial Catalog=NORTHWND;User ID=NORTHWND;Password=133tcru@NOS");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM GHX_EMPLOYEESINFO", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridViewEmployees.DataSource = dt;
+        }
+
+        private void ClearFields()
+        {
+            txtEmployeeID.Clear();
+            txtName.Clear();
+            txtDepartment.Clear();
+            txtDesignation.Clear();
+        }
+
+        private void dataGridViewEmployees_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewEmployees.Rows[e.RowIndex];
+                txtEmployeeID.Text = row.Cells["EmployeeID"].Value.ToString();
+                txtName.Text = row.Cells["EmpName"].Value.ToString();
+                txtDepartment.Text = row.Cells["Department"].Value.ToString();
+                txtDesignation.Text = row.Cells["Designation"].Value.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmployeeID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
